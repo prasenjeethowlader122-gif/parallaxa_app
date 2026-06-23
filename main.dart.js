a5217d8645cf43c1601373372d36a1fd230c676d
prasenjeethowlader122-gif <prasenjeethowlader122@gmail.com>
2026-06-23 05:14:25 +0000

    Deploying to gh-pages from @ prasenjeethowlader122-gif/parallaxa_app@c5430b2e1348428c86e76be6a456744b2c22bd11 🚀

--- main.dart.js
+++ main.dart.js
@@ -54303,7 +54303,7 @@ $S:4}
 A.bMt.prototype={
 $1(a){var s=A.hl().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/77e2e94772b6eb43759e34ed1ad7da4674e19cab/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/a4ce257c68517c1410f4b48ac9852ab5642a3f8d/":s)+a},
 $S:42}
 A.alF.prototype={
 gt(a){var s=this.a
