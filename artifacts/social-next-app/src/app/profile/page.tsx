0db20d3780b2a907746d107c95f75291b5961767
prasenjeethowlader122-gif <prasenjeethowlader122@gmail.com>
2026-08-01 12:59:12 +0600

    Delete artifacts/social-next-app directory

--- artifacts/social-next-app/src/app/profile/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-import ProfilePage from "../user/[userId]/page";
-
-export default function MyProfilePage() {
-  return <ProfilePage />;
-}
--- artifacts/social-next-app/src/app/template.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { AppShell } from "@/components/app-shell";
-
-export default function RootPage({ children }: { children: React.ReactNode }) {
-  return <AppShell>{children}</AppShell>;
-}
